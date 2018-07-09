9661b575d2c85cec39b4bad5dad89cb6e06b0cc9
Hao Weining <samhwn98@gmail.com>
2018-07-09 21:28:35 +0800

    polyhedron formula

--- formula_generator.py
+++ formula_generator.py
@@ -1,4 +1,5 @@
 import formula
+import random
 
 def generate_formula(category, number):
     formulas = formula.Formulas()
@@ -12,14 +13,28 @@ def generate_formula(category, number):
 
     return formulas
 
-
 def generate_polynomial():
     #TODO variable number is up to 10
     print()
 
 def generate_polyhedron():
     #TODO variable number is up to 10
-    print()
+    # generate center point and radius and number of circles
+    num_of_center = random.randint(1, 10)
+    num_of_dimension = random.randint(2, 10)
+    centers = []
+    radiuses = []
+    for i in range(num_of_center):
+        center = []
+        for j in range(num_of_dimension):
+            center_coordinate = random.randint(-1000, 1000)
+            center.append(center_coordinate)
+        radius = random.randint(1,50)
+        centers.append(center)
+        radiuses.append(radius)
+    formula = [centers, radiuses]
+
+    return formula
 
 def generate_specific_formula():
     formulas = formula.Formulas()
@@ -28,3 +43,5 @@ def generate_specific_formula():
     # formulas.put([[[12,0],[-12,0]],[4,4]])
 
     return formulas
+
+print (generate_formula("polyhedron", 5).formulas)
