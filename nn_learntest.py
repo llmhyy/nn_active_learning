c5d060b56bc731b163585e28874b4f5bc9f647f8
Yun Lin <llmhyy@gmail.com>
2018-08-22 16:08:16 +0800

    demo for active learning with java

--- nn_learntest.py
+++ nn_learntest.py
@@ -1,25 +1,38 @@
+import json
 from sys import stdin
 from sys import stdout
 
-import json
-
-# print("start")
-
-# type = stdin.readline()
+print("start")
+i=0
 
 try:
     while (1):
-        data = stdin.readline()
-        data = data.strip("\n")
+        i = i + 1
+        request_type = stdin.readline()
+        request_type = request_type.strip(" ").strip("\n")
+        print("request", request_type)
+
+        message_body = stdin.readline()
+        message_body = message_body.strip(" ").strip("\n")
+        print("message", message_body)
         # json.dump(data)
-        a = json.loads(data)
-        print("@@PythonStart@@")
+        a = json.loads(message_body)
+
+        # print("@@PythonStart@@")
         print(a)
-        print("@@PythonEnd@@")
+        # print("@@PythonEnd@@")
+
+        if (request_type == '$TRAINING' or request_type == '$SEND_LABEL'):
+            if i>5:
+                print("$TRAINING_FINISH")
+            else:
+                print("$REQUEST_LABEL")
+                print(
+                    "[[{'VALUE': '1', 'TYPE': 'PRIMITIVE', 'NAME': 'a'}, {'VALUE': '2', 'TYPE': 'PRIMITIVE', 'NAME': 'b'}], [{'VALUE': '3', 'TYPE': 'PRIMITIVE', 'NAME': 'a'}, {'VALUE': '4', 'TYPE': 'PRIMITIVE', 'NAME': 'b'}], [{'VALUE': '5', 'TYPE': 'PRIMITIVE', 'NAME': 'a'}, {'VALUE': '6', 'TYPE': 'PRIMITIVE', 'NAME': 'b'}]]")
+
         stdout.flush()
-        print("finished!")
-
+except Exception as e:
+    print(e)
 finally:
-# sess.close()
+    # sess.close()
     print("finished!")
-
