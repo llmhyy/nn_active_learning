f5eb5bf2bf6da5a03d2826aef4b3d0aea6e406be
Yun Lin <llmhyy@gmail.com>
2018-09-26 00:13:05 +0800

    add back the benchmark

--- specific_formula_evaluation.py
+++ specific_formula_evaluation.py
@@ -20,7 +20,8 @@ def generate_specific_formula():
     formulas = formula.Formulas()
     formu = formula.Formula(
         # [[[-2, 60], [163, -899]], [485, 430]], formula.POLYHEDRON)
-        [[[-700, -700], [700, 700]], [300, 300]], formula.POLYHEDRON)
+        #[[[-700, -700], [700, 700]], [300, 300]], formula.POLYHEDRON)
+        [[[-571, 31]], [445]], formula.POLYHEDRON)
         # [[[0, 0]], [500]], formula.POLYHEDRON)
     formulas.put(formu.get_category(), formu)
     # formulas.put([[[12,0],[-12,0]],[4,4]])
@@ -45,12 +46,12 @@ train_data_file, test_data_file = data_point_generation.generate_data_points(f, 
 # train_data_file = "dataset/train485_430.csv"
 # test_data_file = "dataset/test485_430.csv"
 print(f.get_list())
-tf.reset_default_graph()
-random.seed(random_seed)
-np.random.seed(random_seed)
-tf.set_random_seed(random_seed)
-
-X = [[-10, -10], [-8, -5], [-11, -6], [0, 10], [1, 14], [-1, 9], [89, 55], [68, 86]]
+# tf.reset_default_graph()
+# random.seed(random_seed)
+# np.random.seed(random_seed)
+# tf.set_random_seed(random_seed)
+#
+# X = [[-10, -10], [-8, -5], [-11, -6], [0, 10], [1, 14], [-1, 9], [89, 55], [68, 86]]
 # while True:
 #     still_on_one_side, X = cluster.get_clustering_points(X, True, f)
 #     if not still_on_one_side:
@@ -75,7 +76,11 @@ train_set_X = []
 train_set_Y = []
 test_set_X = []
 test_set_Y = []
-mid_list = mal.generate_accuracy([], [], train_data_file, test_data_file, f, category, learning_rate, training_epochs, lower_bound, upper_bound, parts_num, True, "", "", True)
+
+benchmark.generate_accuracy(train_data_file, test_data_file, learning_rate, training_epochs, lower_bound, upper_bound)
+
+
+# mid_list = mal.generate_accuracy([], [], train_data_file, test_data_file, f, category, learning_rate, training_epochs, lower_bound, upper_bound, parts_num, True, "", "", True)
 # tf.reset_default_graph()
 # random.seed(random_seed)
 # np.random.seed(random_seed)
