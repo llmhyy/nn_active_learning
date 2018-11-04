ca4a6fc501a88c70c7f1703f55de23a58182eda9
Yun Lin <llmhyy@gmail.com>
2018-11-04 19:51:33 +0800

    minor

--- main/cluster.py
+++ main/cluster.py
@@ -1,7 +1,6 @@
 import math
 
 import numpy as np
-# import matplotlib.pyplot as plt
 from sklearn.cluster import AgglomerativeClustering
 
 
--- main/util.py
+++ main/util.py
@@ -5,7 +5,7 @@ import random
 
 import numpy as np
 import tensorflow as tf
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 from main import label_tester as lt
 from prj_test import formula
