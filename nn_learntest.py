ca01aa873e8497885f8f3224454677b9b248361b
Yun Lin <llmhyy@gmail.com>
2018-11-09 02:12:26 +0800

    clean legacy code

--- nn_learntest.py
+++ nn_learntest.py
@@ -7,8 +7,7 @@ from sys import stdout
 import tensorflow as tf
 
 import util
-from main import label_tester as lt, mid_point_active_learning, json_handler, communication, boundary_exploration as be, \
-    info_checker as ic
+from main import label_tester as lt, mid_point_active_learning, json_handler, communication, boundary_exploration as be
 
 lower_bound = -1000
 upper_bound = 1000
@@ -35,14 +34,13 @@ try:
                 = json_handler.parse_training_message_body(message_body)
             model_folder = os.path.join("models", model_folder)
             label_tester = lt.CoverageLabelTester(variables)
-            info_checker = ic.CoverageInfoChecker(variables)
 
             train_set_x = util.convert_with_mask(train_set_x, train_set_x_info)
             mid_point_learner = mid_point_active_learning.MidPointActiveLearner(train_set_x_info, train_set_x,
                                                                                 train_set_y, None, None,
                                                                                 learning_rate, training_epochs,
                                                                                 lower_bound, upper_bound, False,
-                                                                                label_tester, info_checker,
+                                                                                label_tester,
                                                                                 point_number_limit, model_folder,
                                                                                 model_file_name)
             mid_point_learner.generate_accuracy()
@@ -52,9 +50,9 @@ try:
             data_set = util.convert_with_mask(data_set, data_set_info)
             model_folder = os.path.join("models", model_folder)
             label_tester = lt.CoverageLabelTester(variables)
-            info_checker = ic.CoverageInfoChecker(variables)
 
-            boundary_explorer = be.BoundaryExplorer(data_set_info, data_set, model_folder, model_file_name, label_tester, info_checker, 3)
+            boundary_explorer = be.BoundaryExplorer(data_set_info, data_set, model_folder, model_file_name,
+                                                    label_tester, 3)
             boundary_explorer.boundary_explore()
             communication.send_exploration_finish_message()
         elif request_type == "$MODEL_CHECK":
